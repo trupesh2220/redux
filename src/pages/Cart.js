564908b3d0ac39725f7911aa7eed74bd99c211e7
TRUPESH PADSHALA <123795991+trupesh2220@users.noreply.github.com>
2023-09-01 11:06:56 +0530

    Product Remove from Cart is SuccessFull

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { remove } from "../store/cartSlice";
 function Cart() {
+  const products = useSelector((state) => state.cart);
+  const dispatch = useDispatch()
+  console.log(products);
+  const handelRemove =(id)=>{
+    dispatch(remove(id))
+  }
   return (
     <div>
-      Cart
+      <h3>cart</h3>
+      <div className="cartWrapper">
+        {products.map((product) => (
+          
+          <div className="cartCard" key={product.id}>
+            <img src={product.image} alt="" />
+            <h5>{product.title}</h5>
+            <h5>{product.price}</h5>
+            <button className="btn" onClick={()=>handelRemove(product.id)}>Remove</button>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
